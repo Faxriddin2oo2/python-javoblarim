0d66e917c64d07a84739041b244c04519f66c9a4
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-20 14:54:32 +0500

    Arifmetik amallar

--- AllTasksInOne.py
+++ AllTasksInOne.py
@@ -1,1 +1,25 @@
 # InshaAllah today 19.12.2024 this is will be start of my path in data science sphere
+#03 PRINT(), SINTEKS VA ARIFMETIK AMALLAR
+
+# Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+# "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+# print('"Nexia",', '"Tico",',"'Damas'", "ko'rganlar qilar havas")
+
+# Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
+# 5 ning 4-darajasini toping
+# print("5 ning 4-darajasi:", 5**4)
+
+# 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+# print("22 ni 4 ga bo'lganda:", 22%4, "qoldiq qoladi")
+
+# Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+# print("Tomonlari 125 ga teng kvadratning yuzi:", 125**2, ", perimetri esa:", 125*4)
+
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping  (π=3.14 deb oling)
+# print("Diametri 12 ga teng bo'lgan doirani yuzi:", 3.14*((12/2)**2))
+
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+# print("Katetlari 6 va 7 bo'lgam to'g'ri burchakli uchburchakning gipotenuzasi:", (6**2+7**2)**0.5)
+
+
+
