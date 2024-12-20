c1ec1f309ea61df38ac58f61f6c470e7af73a94a
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-20 18:15:30 +0500

    List bilan amallar

--- AllTasksInOne.py
+++ AllTasksInOne.py
@@ -122,11 +122,11 @@
 # suxbat qurishni xoxlar edim")
 
 # friends nomli bo'sh ro'yxat tuzing va unga .append() yordamida 5-6 ta mehmonga chaqirmoqchi bo'lgan do'stlaringizni kiriting.
-friends = []
-friends.append("Sardor")
-friends.append("Boymurod")
-friends.append("Davlatyor")
-friends.append("Husan")
+# friends = []
+# friends.append("Sardor")
+# friends.append("Boymurod")
+# friends.append("Davlatyor")
+# friends.append("Husan")
 # print(friends)
 
 # Yuqoridagi ro'yxatdan mehmonga kela olmaydigan odamlarni .remove() metodi yordamida o'chrib tashlang. 
@@ -134,15 +134,78 @@ friends.append("Husan")
 # print(friends)
 
 # Ro'yxatning oxiriga, boshiga va o'rtasiga yangi ismlar qo'shing.
-friends.insert(0, 'Xasan')
-friends.insert(3, "Davlatyor")
-friends.insert(-1, 'Zohirjon')
-print(friends)
+# friends.insert(0, 'Xasan')
+# friends.insert(3, "Davlatyor")
+# friends.insert(-1, 'Zohirjon')
+# print(friends)
 
 # Yangi mehmonlar deb nomlangan bo'sh ro'yxat yarating. .pop() va .append() metodlari yordamida mehmonga kelgan 
 # do'stlaringizning ismini friends ro'yxatidan sug'urib olib, mehmonlar ro'yxatiga qo'shing.
-mehmonlar = []
-mehmonlar.append(friends.pop(1))
-mehmonlar.append(friends.pop(-1))
-mehmonlar.append(friends.pop(3))
-print("\n Kelgan mehmonlar: ",mehmonlar)
+# mehmonlar = []
+# mehmonlar.append(friends.pop(1))
+# mehmonlar.append(friends.pop(-1))
+# mehmonlar.append(friends.pop(3))
+# print("\n Kelgan mehmonlar: ",mehmonlar)
+
+
+#08 RO'YXATLAR BILAN ISHLASH
+# AMALIYOT
+# O'zingizga ma'lum davlatlarning ro'yxatini tuzing va ro'yxatni konsolga chiqaring
+davlatlar = ["O'zbekiston", "Saudiya Arabistoni", "Germaniya", "Yaponiya", "Malaysiya", "Indonesiya"]
+# Ro'yxatning uzunligini konsolga chiqaring
+# print(len(davlatlar))
+
+# sorted() funktsiyasi yordamida ro'yxatni tartiblangan holda konsolga chiqaring
+# print(sorted(davlatlar))
+# print(sorted(davlatlar, reverse=True))
+# sorted() yordamida ro'yxatni teskari tartibda konsolga chiqaring
+
+# Asl ro'yxatni qaytadan konsolga chiqaring
+# print(davlatlar)
+
+# reverse() metodi yordamida ro'yxatni ortidan boshlab chiqaring
+# davlatlar.reverse()
+# print(davlatlar)
+
+# sort() metodi yordamida ro'yxatni avval alifbo bo'yicha, keyin esa alifboga teskari tartibda konsolga chiqaring.
+# davlatlar.sort()
+# print(davlatlar)
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+
+# 120 dan 1200 gacha bo'lgan juft sonlar ro'yxatini tuzing
+sonlar = list(range(120,1200,2))
+
+# Ro'yxatdagi sonlar yig'indisini hisoblang va konsolga chiqaring
+# print(sum(sonlar))
+
+# Ro'yxatdagi eng katta va eng kichik son o'rtasidagi ayirmani hisoblang va konsolga chiqaring
+# print(max(sonlar)-min(sonlar))
+
+# Ro'yxatdagi elementlar sonini hisoblang
+# print(len(sonlar))
+
+# Ro'yxatning boshidan, o'rtasidan va oxiridan 20 ta qiymatni konsolga chiqaring
+# boshi = sonlar[:20]
+# urta = sonlar[260:280]
+# oxiri = sonlar[-20:]
+# print(boshi+urta+oxiri)
+
+# taomlar degan ro'yxat yarating va ichiga istalgan 5ta taomni kiriting
+taomlar = ["osh", "manti", "somsa", "beshbarmoq", "lag'mon"]
+
+# nonushta degan yangi ro'yxatga taomlardan nusxa oling
+nonushta = []
+
+# Yangi ro'yxatda faqat nonushtaga yeyiladigan taomlarni qoldiring, va qo'shimcha 2 ta taom qo'shing
+nonushta.append(taomlar.pop(0))
+nonushta.append(taomlar.pop(1))
+nonushta.append("saryog'")
+nonushta.append("qaymoq")
+print(nonushta)
+# Ikkala ro'yxatni ham (taomlar va nonushta) konsolga chiqaring
+
+# Yuqoridagi nonushta ro'yxatini o'zgarmas ro'yxatga aylantiring va nonushta[0] = "qaymoq va non" deb qiymat berib ko'ring.
+nonushta = tuple(nonushta)
+# nonushta[0] = 'qaymoq va non' # We all knew that will be the error
+print(nonushta)
