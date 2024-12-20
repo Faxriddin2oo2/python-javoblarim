b3b473d9ebcc646530abd35cd8696b11c0378d09
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-20 22:30:42 +0500

    bir nechta shartlarni bajarish

--- AllTasksInOne.py
+++ AllTasksInOne.py
@@ -287,3 +287,86 @@
 #     print(f"Bu {son} ning ildizi: {son**0.5}")
 # else:
 #     print("Iltimos musbat son kiriting!")
+
+
+# Bir nechta shartlarni tekshirish
+# AMALIYOT
+# Quyidagi dasturlarni alohida fayllarga yozing va bajaring:
+# Foydalanuvchidan juft son kiritishni so'rang. Agar foydalanuvchi juft son kiritsa "Rahmat!", agar toq son kiritsa "Bu son juft emas" degan xabarni chiqaring.
+# son = int(input("Juft son kiriting: "))
+# if son%2 == 0:
+#     print("Rahmat!")
+# else:
+#     print("Bu juft son emas")
+
+# Foydalanuvchi yoshini so'rang, va muzeyga kirish uchun chipta narhini quyidagicha chiqaring:
+# Agar foydalanuvchi 4 yoshdan kichkina yoki 60 dan katta bo'lsa bepul
+# Agar foydalanuvchi 18 dan kichik bo'lsa 10000 so'm
+# Agar foydalanuvchi 18 dan katta bo'lsa 20000 so'm
+# yosh = int(input("Iltimos yoshingizni kiriting: "))
+# if yosh < 4 or yosh>60:
+#     narx = 'bepul'
+# elif yosh < 18:
+#     narx = 10000
+# else:
+#     narx = 20000
+# print(f"Sizga muzeyga kirish {narx} bo'ladi")
+
+# Foydalanuvchidan ikita son kiritishni so'rang, sonlarni solishtiring va ularning teng yoki katta/kichikligi haqida xabarni chiqaring
+# print("Siz menga ikta son bering va men ularni solishtiraman!")
+# son1 = int(input("Birinchi sonni kiriting: "))
+# son2 = int(input("Ikkinchi sonni kiriting: "))
+# if son1 > son2:
+#     print(f"Birinchi son katta!--> {son1} > {son2}")
+# elif son1 < son2:
+#     print(f"Ikkinchi son katta!--> {son1} < {son2}")
+# else:
+#     print("Bu sonlar teng!")
+
+# mahsulotlar degan ro'yxat yarating va kamida 10 ta turli mahsulotni kiriting.
+#  Yangi, savat degan bo'sh ro'yxat yarating va foydalanuvchidan savatga kamida 5 ta mahsulot kiritishni so'rang. 
+# Savatdagi elementlarni, mahsulotlar ro'yxati bilan solishtiring va qaysi biri ro'yxatda bo'lsa "Mahsulot do'konimizda bor" aks holda, 
+# "Mahsulot do'konimizda yo'q" degan xabarlarni chiqaring.
+# mahsulotlar = ['guruch', 'sabzi', 'piyoz', 'yo\'g', 'go\'sht', 'qalampir', 'zira', 'olma', 'chimchi', 'suv']
+# savat = []
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1} chi mahsulotni soling: "))
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         print(f"{mahsulot.title()} bor bizda")
+#     else:
+#         print(f"Afsuski {mahsulot} bizda yo'q")
+
+# Yuqoridagi dasturni quyidagicha o'zgartiring: foydalanuvchidan 5 ta mahsulot kiritishni so'rang. 
+# Foydalanuvchi so'ragan va do'konda bor mahsulotlarni yangi, bor_mahsulotlar degan ro'yxatga, 
+# do'konda yo'q mahsulotlarni esa mavjud_emas degan ro'yxatga qo'shing.  Agar mavjud_emas ro'yxati bo'sh bo'lsa, 
+# "Siz so'ragan barcha mahsulotlar do'konimizda bor" degan xabarni, aks holda esa "Quyidagi mahsulotlar do'konimizda yo'q: ....." degan xabarni chiqaring.
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+# if mavjud_emas == []:
+#     print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+# else:
+#     print("\nQuyidagi mahsulotlar do'konimizda yo'q: ")
+#     for i in mavjud_emas:
+#         print(i)
+
+# foydalanuvchilar degan ro'yxat tuzing, va kamida 5 ta login qo'shing. 
+# Foydalanuvchidan yangi login tanlashni so'rang va foydalanuvchi kiritgan loginni foydalanuvchilar degan ro'yxatning tarkibi bilan solishtiring. 
+# Agar ro'yxatda bunday login mavjud bo'lsa, "Login band, yangi login tanlang!" aks holda "Xush kelibsiz, foydalanuvchi!" xabarini chiqaring.
+# foydalanuvchilar = ['magneto', 'abu123', 'umar', 'faxriddin', 'ajdar jangchisi']
+# savol = input("Yangi login kiriting:\n>>>")
+# if savol.lower() in foydalanuvchilar:
+#     print("Login band, yangi login tanlang!")
+# else:
+#     print(f"Xush kelibsiz, {savol.title()}")
+
+# Foydalanuvchidan biror butun son kiritishni so'rang. Foydalanuvchi kiritgan sonni 2 da 10 gacha bo'lgan sonlardan qay biriga qoldiqsiz bo'linishini konsolga chiqaring. 
+# son = int(input("Son kiriting: "))
+# for n in range(2,11):
+#     if son%n==0:
+#         print(f"{son} {n} ga qoldiqsiz bo'linadi")
