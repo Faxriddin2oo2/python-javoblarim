b079c28abb4c29c7e4fc24fd0b763015a66864ca
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-20 20:08:56 +0500

    if/else

--- AllTasksInOne.py
+++ AllTasksInOne.py
@@ -241,3 +241,49 @@
 #     odamlar.append(input(f"Ko'rishganiz {n+1} chi odamiz: "))
 
 # print("Siz ko'rishgan odamlar: ",odamlar)
+
+
+# if/else 
+# AMALIYOT
+# Yangi cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia'] degan ro'yxat tuzing, ro'yxat elementlarining birinchi harfini katta qilib konsolga chqaring. 
+# GM uchun ikkala harfni katta qiling.
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car == "gm":
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# Yuqoridagi mashqni teng emas (!=) operatori yordamida bajaring. 
+# for car in cars:
+#     if car != "gm":
+#         print(car.title())
+#     else:
+#         print(car.upper())
+
+# Foydalanuvchi login ismini so'rang. Agar login admin bo'lsa, "Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi?" 
+# xabarini konsolga chiqaring. Aks holda, "Xush kelibsiz, {foydalanuvchi_ismi}!"  matnini konsolga chiqaring.
+# savol = input("Iltimos ismingizni kiriting: ")
+# if savol.lower() == 'admin':
+#     print("Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi?")
+# else:
+#     print(f"Xush kelibsiz, {savol.title()}")
+
+# Foydalanuvchidan 2 ta son kiritishni so'rang. Agar ikki son bir-biriga teng bo'lsa, "Sonlar teng" ekan degan yozuvni konsolga chiqaring.
+# son1 = int(input("Iltimos birinchi sonni kiriting: "))
+# son2 = int(input("Iltimos ikkinchi sonni kiriting: "))
+# if son1 == son2:
+#     print("Bu ikta son teng ekan!")
+
+# Foydalanuvchidan istalgan son kiritishni so'rang. Agar son manfiy bo'lsa konsolga "Manfiy son", agar musbat bo'lsa "Musbat son" degan xabarni chiqaring. 
+# son = int(input("Istalgan sonni kiriting: "))
+# if son < 0:
+#     print("Bu manfiy son")
+# else:
+#     print("Bu musbat son")
+
+# Foydalanuvchidan son kiritishni so'rang, agar son musbat bo'lsa uning ildizini hisoblab konsolga chiqaring. Agar son manfiy bo'lsa, "Musbat son kiriting" degan xabarni chiqaring. 
+# if son >= 0:
+#     print(f"Bu {son} ning ildizi: {son**0.5}")
+# else:
+#     print("Iltimos musbat son kiriting!")
