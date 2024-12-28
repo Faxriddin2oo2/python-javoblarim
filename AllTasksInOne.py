964fa9a21600b97fab170d9800ae053d9012f8fd
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-28 21:29:44 +0500

    uptades

--- AllTasksInOne.py
+++ AllTasksInOne.py
@@ -888,14 +888,14 @@ import re
 # print(re.match(andoza, savol))
 
 # Berilgan matndan veb sahifa manzilini ajratib olyuvchi funksiya yozing. Quyidagi matndan namuna sifatida foydalanishingiz mumkin:
-def extract_urls(text):
-    # Simple regex pattern for URLs
-    pattern = r'http[s]?://\S+'
-    return re.findall(pattern, text)
+# def extract_urls(text):
+#     # Simple regex pattern for URLs
+#     pattern = r'http[s]?://\S+'
+#     return re.findall(pattern, text)
 
-# Example usage
-text = "Assalom alaykum hurmatli do'stlar. Navbatdagi darsimiz YouTubega yuklandi: https://youtu.be/vsxJPRLXpgI \
-# Ushbu darsimizda unittest moduli yordamida klasslarning xususiyatlar va metodlarini tekshiruvchi dastur yozishni o'rganamiz. \
-# Bugungi dars manzili: https://python.sariq.dev/testing/37-klass-test"
-urls = extract_urls(text)
-print(urls)
+# # Example usage
+# text = "Assalom alaykum hurmatli do'stlar. Navbatdagi darsimiz YouTubega yuklandi: https://youtu.be/vsxJPRLXpgI \
+# # Ushbu darsimizda unittest moduli yordamida klasslarning xususiyatlar va metodlarini tekshiruvchi dastur yozishni o'rganamiz. \
+# # Bugungi dars manzili: https://python.sariq.dev/testing/37-klass-test"
+# urls = extract_urls(text)
+# print(urls)
