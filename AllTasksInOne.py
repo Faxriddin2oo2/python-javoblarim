aaefa56c0ebc74e2e8d7db6c6116e5cb80537f4a
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-29 22:07:27 +0500

    uptades

--- AllTasksInOne.py
+++ AllTasksInOne.py
@@ -894,8 +894,8 @@ import re
 #     return re.findall(pattern, text)
 
 # # Example usage
-# text = "Assalom alaykum hurmatli do'stlar. Navbatdagi darsimiz YouTubega yuklandi: https://youtu.be/vsxJPRLXpgI \
-# # Ushbu darsimizda unittest moduli yordamida klasslarning xususiyatlar va metodlarini tekshiruvchi dastur yozishni o'rganamiz. \
-# # Bugungi dars manzili: https://python.sariq.dev/testing/37-klass-test"
-# urls = extract_urls(text)
-# print(urls)
+text = "Assalom alaykum hurmatli do'stlar. Navbatdagi darsimiz YouTubega yuklandi: https://youtu.be/vsxJPRLXpgI \
+# Ushbu darsimizda unittest moduli yordamida klasslarning xususiyatlar va metodlarini tekshiruvchi dastur yozishni o'rganamiz. \
+# Bugungi dars manzili: https://python.sariq.dev/testing/37-klass-test"
+urls = extract_urls(text)
+print(urls)
